d682e7b70755e5f2f71a0e2eefcfb358e9abfe01
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-06-23 18:04:25 -0400

    added thought routes and reaaction routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
+const { addThought, removeThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
 
+// /api/comments/<pizzaId>
+router.route('/:userId')
+    .post(addThought);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+    .route('/:userId/:thoughtId')
+    .put(addReaction)
+    .delete(removeThought);
+
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
 
 module.exports = router;
