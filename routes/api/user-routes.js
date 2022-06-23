a1ae80c6a64340001329b0a85c1c730a935d77c3
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-06-23 17:19:52 -0400

    added user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const { getAllUser, getUserById, createUser, updateUser, deleteUser } = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/Users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
